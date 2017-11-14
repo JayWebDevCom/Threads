13767bfcac6d2bc1801e74138984042bd69167dc
JayWebDevCom <jaiye.s@gmail.com>
2017-11-14 23:45:56 +0000

    Create an instance of another thread in an anonymous class w & w/o lambda

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        // threads don't always run in the expected order
+        // can't start an instance of a thread more than once or one that is already running
+        // will throw an exception
+
         System.out.println("Written in the main thread");
 
         Thread anotherThread = new AnotherThread();
         anotherThread.start();
 
+        // anonymous class must be started immediately
+        new Thread() {
+            public void run(){
+                System.out.println("Written in anonymous class thread");
+            }
+        }.start();
+
+        // anonymous class lambda invocation
+        new Thread(() -> System.out.println("Written in second anonymous class thread")).run();
+
         System.out.println("Written again in the main thread");
+
     }
 }

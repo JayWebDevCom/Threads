4de4037ec27904b4e35e2326772e5f7fd56b5e1b
JayWebDevCom <jaiye.s@gmail.com>
2017-11-16 22:13:54 +0000

    Introduce sleep() in Another Thread

--- src/com/company/AnotherThread.java
+++ src/com/company/AnotherThread.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import static com.company.ThreadColour.ANSI_BLUE;
+
 public class AnotherThread extends Thread {
     @Override
     public void run() {
-        System.out.println(ThreadColour.ANSI_BLUE +  "Written in another thread");
+        System.out.println(ANSI_BLUE +  "Written in another thread");
+        System.out.println(ANSI_BLUE +  "Written in: " + currentThread().getName());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            // exception if another thread wakes this one up
+            System.out.printf("%sAnother thread woke me up, I am: %s%n", ANSI_BLUE, currentThread().getName());
+            e.printStackTrace();
+        }
+
+        System.out.println(ANSI_BLUE +  "3 secs have passed - sleep over!");
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,47 @@
 package com.company;
 
+import static com.company.ThreadColour.ANSI_CYAN;
+import static com.company.ThreadColour.ANSI_GREEN;
+import static com.company.ThreadColour.ANSI_RED;
+
 public class Main {
 
     public static void main(String[] args) {
         // threads don't always run in the expected order
         // can't start an instance of a thread more than once or one that is already running
         // will throw an exception
+        // call start() not run()
 
         System.out.println(ThreadColour.ANSI_PURPLE + "Written in the main thread");
 
         Thread anotherThread = new AnotherThread();
+        anotherThread.setName("**Another Thread**");
         anotherThread.start();
 
         // anonymous class must be started immediately
         new Thread() {
             public void run() {
-                System.out.println(ThreadColour.ANSI_GREEN + "Written in anonymous class thread");
+                System.out.println(ANSI_GREEN + "Written in anonymous class thread");
+                System.out.println(ANSI_GREEN +  "Written in: " + currentThread().getName());
             }
         }.start();
+
         // anonymous class lambda invocation
-        new Thread(() -> System.out.println(ThreadColour.ANSI_CYAN + "Written in second anonymous class thread")).run();
+        new Thread(() ->
+                System.out.println(ANSI_CYAN + "Written in second anonymous class thread")
+        ).start();
 
         // run method from MyRunnable Thread with both run methods
         Thread myRunnable = new Thread(new MyRunnable(){
             @Override
             public void run() {
                 super.run();
-                System.out.println(ThreadColour.ANSI_RED + "Written in Anon Class MyRunnable implementation of Run...");
+                System.out.println(ANSI_RED + "Written in Anon Class MyRunnable implementation of Run...");
             }
         });
 
         myRunnable.start();
 
-        System.out.println(ThreadColour.ANSI_PURPLE + "Written again in the main thread");
+        System.out.printf("%sWritten again in the main thread%n", ThreadColour.ANSI_PURPLE);
     }
 }
